cda9e3630c869b8dda644032ae3600e33e1b358e
Three <36061421+ThreeSR@users.noreply.github.com>
2020-11-27 11:35:21 +0800

    Create pair.cpp

---
+++ pair.cpp
@@ -0,0 +1,14 @@
+pair<int ,double> p1;
+ 
+p1.first = 1;
+ 
+p1.second = 2.5;
+ 
+cout<<p1.first<<' '<<p1.second<<endl;
+ 
+//输出结果：1 2.5
+ 
+ 
+string firstBook;
+if(author.first=="James" && author.second=="Joy")
+    firstBook="Stephen Hero";
